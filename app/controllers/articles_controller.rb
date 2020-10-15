012c35d7a7ae0e6e4b3245fa6158e734dfc05a6f
shibaaa <k.shibata9447@gmail.com>
2020-10-15 19:24:02 +0900

    ログイン前のトップページを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:destroy]
+
   def index
+    if user_signed_in?
+      "ブックマーク一覧"
+    else
+      render "welcome/index"
+    end
   end
 
   def destroy
