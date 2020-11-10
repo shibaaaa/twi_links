db1cadb24c243004a61e8f5a6939786fac1e4758
shibaaa <k.shibata9447@gmail.com>
2020-11-10 18:48:13 +0900

    フッターの作成
    フッター作成に伴いプライバシーポリシーと免責事項のページとルーティングを追加

---
+++ app/controllers/disclaimers_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class DisclaimersController < ApplicationController
+  layout "welcome"
+
+  def index
+  end
+end
---
+++ app/controllers/privacy_policies_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class PrivacyPoliciesController < ApplicationController
+  layout "welcome"
+
+  def index
+  end
+end
