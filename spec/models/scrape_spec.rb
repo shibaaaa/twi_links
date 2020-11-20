56f5e9c68728bd135f6eb9f04fa6aec23fba1a84
shibaaa <k.shibata9447@gmail.com>
2020-11-20 17:21:11 +0900

    Scrapeモデルのモデルスペックを書いた

---
+++ spec/models/scrape_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Scrape, type: :model do
+  describe "::fetch_title" do
+    it "URLからタイトルを取得できること" do
+      page = Scrape.access_page("http://example.com/")
+      expect(Scrape.fetch_title(page)).to eq "Example Domain"
+    end
+  end
+
+  describe "::fetch_og_image" do
+    context "任意のページにog:imageタグが存在するとき" do
+      it "og:imageタグの要素を取得できること" do
+        page = Scrape.access_page("https://shibaaa647.hatenablog.com/")
+        expect(Scrape.fetch_og_image(page)).to eq "https://cdn.blog.st-hatena.com/images/theme/og-image-1500.png"
+      end
+    end
+
+    context "任意のページog:imageタグが存在しないとき" do
+      it "'no_image.svg'が返ること" do
+        page = Scrape.access_page("http://example.com/")
+        expect(Scrape.fetch_og_image(page)).to eq "no_image.svg"
+      end
+    end
+  end
+end
