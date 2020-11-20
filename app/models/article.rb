ec0de29207017211bdae21ef63d941f4b196093e
shibaaa <k.shibata9447@gmail.com>
2020-11-20 16:36:37 +0900

    スクレイピングの処理をクラスに切り出した

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,11 +8,28 @@ class Article < ApplicationRecord
       article_params = Parallel.map(fetch_liked_tweets(user), in_threads: 5) do |tweet|
         if tweet.uris?
           article_url = tweet.uris.first.expanded_url.to_s
-          page = mechanize_agent.get(article_url)
+
+          begin
+            page = Scrape.access_page(article_url)
+            article_title = Scrape.fetch_title(page)
+            article_image = Scrape.fetch_og_image(page)
+          rescue Mechanize::ResponseCodeError => e
+            if e.response_code = 404
+              next
+            else
+              article_title = "Not Found"
+              article_image = "no_image.svg"
+            end
+          rescue => e
+            ErrorUtility.log e
+            article_title = "Not Found"
+            article_image = "no_image.svg"
+          end
+
           {
             url: article_url,
-            title: fetch_title(page),
-            image_meta: fetch_og_image(page),
+            title: article_title,
+            image_meta: article_image,
             user_id: user.id,
             tweet_id: tweet.id,
             tweeted_at: tweet.created_at,
@@ -39,27 +56,6 @@ class Article < ApplicationRecord
           config.access_token_secret = user.access_token_secret
         end
         client
-      end
-
-      def mechanize_agent
-        agent = Mechanize.new
-        agent.user_agent_alias = "Windows Chrome"
-        agent
-      end
-
-      def fetch_title(page)
-        page.title || "No Title"
-      rescue => e
-        ErrorUtility.log_and_notify e
-        "No Title"
-      end
-
-      def fetch_og_image(page)
-        og_image = page.at('meta[property="og:image"]')
-        og_image ? og_image[:content] : "no_image.svg"
-      rescue => e
-        ErrorUtility.log_and_notify e
-        "no_image.svg"
       end
 
       def fetch_liked_tweets(user)
---
+++ app/models/scrape.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class Scrape
+  class << self
+    def access_page(url)
+      mechanize_agent.get(url)
+    end
+
+    def fetch_title(page)
+      page.title || "No Title"
+    rescue => e
+      ErrorUtility.log e
+      "Not Found"
+    end
+
+    def fetch_og_image(page)
+      og_image = page.at('meta[property="og:image"]')
+      og_image ? og_image[:content] : "no_image.svg"
+    rescue => e
+      ErrorUtility.log e
+      "no_image.svg"
+    end
+
+    private
+      def mechanize_agent
+        agent = Mechanize.new
+        agent.user_agent_alias = "Windows Chrome"
+        agent
+      end
+    end
+end
