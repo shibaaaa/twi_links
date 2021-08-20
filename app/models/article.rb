b54ca645dd21e85441744ed861d2e5114b5c4201
Kosuke Shibata <k.shibata9447@gmail.com>
2021-08-20 10:16:32 +0900

    articleモデルのループ内で使用するスクレイピングのメソッドをクラスメソッド化
    ループ内でインスタンス化していてパフォーマンスが悪いため

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,42 +5,42 @@ class Article < ApplicationRecord
 
   class << self
     def insert_from_tweets(user)
+      crawler = Scrape.mechanize_agent
       article_params = Parallel.map(fetch_liked_tweets(user), in_threads: 5) do |tweet|
-        if tweet.uris?
-          article_url = tweet.uris.first.expanded_url.to_s
-          crawler = Scrape.new(article_url)
+        next if tweet.uris.blank?
+        article_url = tweet.uris.first.expanded_url.to_s
 
-          begin
-            page = crawler.access_page
-            article_title = crawler.fetch_title(page)
-            article_image = crawler.fetch_og_image(page)
-          rescue Mechanize::ResponseCodeError => e
-            if e.response_code == "404"
-              next
-            else
-              article_title = "Not Found"
-              article_image = "no_image.svg"
-            end
-          rescue => e
-            ErrorUtility.log e
+        begin
+          page = Scrape.access_page(crawler, article_url)
+          article_title = Scrape.fetch_title(page)
+          article_image = Scrape.fetch_og_image(page)
+        rescue Mechanize::ResponseCodeError => e
+          if e.response_code == "404"
+            next
+          else
             article_title = "Not Found"
             article_image = "no_image.svg"
           end
-
-          {
-            url: article_url,
-            title: article_title,
-            image_meta: article_image,
-            user_id: user.id,
-            tweet_id: tweet.id,
-            tweeted_at: tweet.created_at,
-            tweet_url: tweet.url.to_s,
-            tweet_user_meta: tweet.user.profile_image_url_https.to_s,
-            updated_at: Time.current,
-            created_at: Time.current
-          }
+        rescue => e
+          ErrorUtility.log e
+          article_title = "Not Found"
+          article_image = "no_image.svg"
         end
+
+        {
+          url:             article_url,
+          title:           article_title,
+          image_meta:      article_image,
+          user_id:         user.id,
+          tweet_id:        tweet.id,
+          tweeted_at:      tweet.created_at,
+          tweet_url:       tweet.url.to_s,
+          tweet_user_meta: tweet.user.profile_image_url_https.to_s,
+          updated_at:      Time.current,
+          created_at:      Time.current
+        }
       end
+
       Article.insert_all(article_params.compact, unique_by: :url)
     end
 
