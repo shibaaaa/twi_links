8d95b500e71f11e7349a39ba8e36276aad6689b6
Kosuke Shibata <k.shibata9447@gmail.com>
2021-08-25 15:11:05 +0900

    Scrapeモデル内での例外処理を削除
    呼び出し元で例外処理している

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -17,17 +17,11 @@ class Scrape
 
     def fetch_title(page)
       page.title || "No Title"
-    rescue => e
-      ErrorUtility.log e
-      "Not Found"
     end
 
     def fetch_og_image(page)
       og_image = page.at('meta[property="og:image"]')
       og_image ? og_image[:content] : "no_image.svg"
-    rescue => e
-      ErrorUtility.log e
-      "no_image.svg"
     end
   end
 end
