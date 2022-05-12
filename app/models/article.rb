48829bdc2e596a5ea8476eced74bdc27a9e92dce
Kosuke Shibata <k.shibata9447@gmail.com>
2022-05-12 09:26:40 +0900

    Articleオブジェクトのbuildをプライベートメソッドに切り出した

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,28 +12,32 @@ class Article < ApplicationRecord
 
         begin
           page = Scraper.access_page(article_url)
-          article_title = Scraper.fetch_title(page)
-          article_image = Scraper.fetch_og_image(page)
         rescue => e
           next if e.class == Timeout::Error || e.response_code == "404"
           ErrorUtility.log e
         end
 
-        article_params << {
+        article_params << self.build_params(article_url, page, user_id, tweet)
+      end
+
+      Article.insert_all(article_params, unique_by: :url)
+    end
+
+    private
+      def build_params(article_url, page, user_id, tweet)
+        now = Time.current
+        {
           url:             article_url,
-          title:           article_title,
-          image_meta:      article_image,
+          title:           Scraper.fetch_title(page),
+          image_meta:      Scraper.fetch_og_image(page),
           user_id:,
           tweet_id:        tweet.id,
           tweeted_at:      tweet.created_at,
           tweet_url:       tweet.url.to_s,
           tweet_user_meta: tweet.user.profile_image_url_https.to_s,
-          updated_at:      Time.current,
-          created_at:      Time.current
+          updated_at:      now,
+          created_at:      now
         }
       end
-
-      Article.insert_all(article_params, unique_by: :url)
-    end
   end
 end
